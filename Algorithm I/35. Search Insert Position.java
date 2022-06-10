206542bb707aca5a4d77ca57b63ac7b08f79bf60
Parth Vasant Kataria <kataria.p@northeastern.edu>
2022-06-10 16:12:58 -0400

    10062002

---
+++ Algorithm I/35. Search Insert Position.java
@@ -0,0 +1,34 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int right = nums.length - 1, left = 0, pivot = 0;
+        while (left <= right) {
+            pivot = left + (right - left) / 2;
+            if (target == nums[pivot]) {
+                return pivot;
+            }
+            if (target < nums[pivot]) {
+                right = pivot - 1;
+            } else {
+                left = pivot + 1;
+            }
+        }
+        return left;
+    }
+}
